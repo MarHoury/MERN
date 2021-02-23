3dd4b4e4458d1c811d61ee419fd1fc4f354ac11b
MarHoury <marhoury@hotmail.com>
2021-02-23 17:55:05 +1300

    Add MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30MB", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30MB", extended: true}));
+app.use(bodyParser.json({ limit: "30MB", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30MB", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://haoyu-ma_mern:haoyu-ma_mern@cluster0.aefzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
