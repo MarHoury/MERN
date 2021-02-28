7c23dd040b4ede3e6f4d4133b1709b9ca3b4ab4b
MarHoury <marhoury@hotmail.com>
2021-03-01 12:57:05 +1300

    Bugfixed

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import { useDispatch } from 'react-redux';
 
 import defaultBg from '../../../images/default.jpg';
 import useStyles from './styles';
-import { deletePost, likePost } from '../../../api';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
@@ -31,15 +31,15 @@ const Post = ({ post, setCurrentId }) => {
       </div>
       <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>{post.message}</Typography>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions} >
-        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+        <Button size="small" color="primary" onClick={() => { dispatch(likePost(post._id)) }}>
           <ThumbUpAltIcon fontSize="small" />
-          Like
+          &nbsp;Like&nbsp;
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+        <Button size="small" color="primary" onClick={() => { dispatch(deletePost(post._id)) }}>
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
